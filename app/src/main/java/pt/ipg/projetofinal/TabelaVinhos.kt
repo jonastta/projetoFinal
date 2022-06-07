6964b6d2063a440a8f7c1e19f0afafc2dea57e1d
João Costa <jonas789@outLook.com>
2022-06-07 18:51:45 +0100

    Criação TabelaVinhos

---
+++ app/src/main/java/pt/ipg/projetofinal/TabelaVinhos.kt
@@ -0,0 +1,45 @@
+package pt.ipg.projetofinal
+
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaVinhos(db: SQLiteDatabase){
+    private val db: SQLiteDatabase = db
+
+    fun cria(){
+        db.execSQL("CREATE TABLE $NOME_TABELA ( ${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME_VINHO TEXT NOT NULL, $CAMPO_COR TEXT NOT NULL, $CAMPO_TEOR FLOAT NOT NULL )" )
+    }
+
+    fun insert(values: ContentValues): Long {
+        return db.insert(NOME_TABELA,null, values)
+    }
+
+    fun update(values: ContentValues, whereClause: String, whereArgs: Array<String>): Int {
+        return db.update(NOME_TABELA, values, whereClause, whereArgs)
+    }
+
+    fun delete(whereClause: String, whereArgs: Array<String>): Int {
+        return db.delete(NOME_TABELA,whereClause, whereArgs)
+    }
+
+    fun query(
+        columns: Array<String>,
+        selection: String,
+        selectionArgs: Array<String>,
+        groupBy: String,
+        having: String,
+        orderBy: String
+    ): Cursor? {
+        return db.query(NOME_TABELA, columns, selection, selectionArgs, groupBy, having, orderBy)
+    }
+
+    companion object{
+        const val NOME_TABELA = "vinhos"
+        const val CAMPO_NOME_VINHO = "nome"
+        const val CAMPO_COR = "cor"
+        const val CAMPO_TEOR = "teor"
+
+    }
+}
